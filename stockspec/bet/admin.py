a8fb40b4493ed8638c822bd079e8dc329b87e4f3
Thomas Gak Deluen <tgdn45@gmail.com>
2020-11-15 11:54:33 +0100

    Remove unused imports

--- stockspec/bet/admin.py
+++ stockspec/bet/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from django.utils.translation import gettext, gettext_lazy as _
 
 from stockspec.bet.models import Bet
 
